14defd4ca9bf49db070ecd46e898d65277543797
Oliver Naumann <bldrax@gmail.com>
2025-09-23 19:26:45 +0200

    Add an initial leaderboard

---
+++ src/autoloads/highscores.gd
@@ -0,0 +1,100 @@
+# To be used as autoload, class name intentionally omitted.
+extends Node
+
+class ScoreEntry:
+	var score_value : int = 0
+	var name : String = ""
+
+const _HIGHSCORES_PATH : String = "user://highscores.cfg"
+const _MAX_NUM_ENTRIES : int = 3
+
+var highscores : Array[ScoreEntry] = []
+
+
+func add_score_entry(entry: ScoreEntry) -> void:
+	# Prune name to three characters:
+	entry.name = entry.name.substr(0, 3)
+	# Make name all upper case:
+	entry.name = entry.name.to_upper()
+
+	highscores.append(entry)
+	highscores.sort_custom(func(a, b): return a.score_value > b.score_value)
+	if highscores.size() > _MAX_NUM_ENTRIES:
+		highscores = highscores.slice(0, _MAX_NUM_ENTRIES)
+
+
+func reset_highscores() -> void:
+	highscores.clear()
+
+	var entry1 = ScoreEntry.new()
+	entry1.score_value = 100000
+	entry1.name = "ZPX"
+	highscores.append(entry1)
+
+	var entry2 = ScoreEntry.new()
+	entry2.score_value = 10000
+	entry2.name = "AST"
+	highscores.append(entry2)
+
+	var entry3 = ScoreEntry.new()
+	entry3.score_value = 1000
+	entry3.name = "ENV"
+	highscores.append(entry3)
+
+
+func is_top_three_score(score : int) -> bool:
+	if highscores.size() < 3:
+		return true
+
+	return score > highscores[2].score_value
+
+
+func read_highscores_file() -> void:
+	var highscores_file = ConfigFile.new()
+	var error : Error = highscores_file.load(_HIGHSCORES_PATH)
+	if error != OK:
+		push_warning("Not loading highscores file, doesn't exist. Creating. This is expected on first run.")
+		highscores_file.save(_HIGHSCORES_PATH)
+		reset_highscores()
+		return
+
+	# Gather all entries by index until missing
+	for i in _MAX_NUM_ENTRIES:
+		var key_score_value = "entry_%d_score_value" % i
+		var key_name = "entry_%d_name" % i
+		if not highscores_file.has_section_key("highscores", key_score_value):
+			push_warning("Highscores file is malformed: entry %d missing score value. Not loading." % i)
+			reset_highscores()
+			return
+		if not highscores_file.has_section_key("highscores", key_name):
+			push_warning("Highscores file is malformed: entry %d missing name. Not loading." % i)
+			reset_highscores()
+			return
+
+		var entry = ScoreEntry.new()
+		entry.score_value = highscores_file.get_value("highscores", key_score_value)
+		entry.name = highscores_file.get_value("highscores", key_name)
+		add_score_entry(entry)
+	
+	assert(highscores.size() == _MAX_NUM_ENTRIES)
+
+
+func save_highscores_file() -> void:
+	var highscores_file = ConfigFile.new()
+	var error : Error = highscores_file.load(_HIGHSCORES_PATH)
+	if error != OK:
+		push_error("Cannot save highscores, file expected but doesn't exist. Creating new file.")
+
+	# Clear previous entries in the section
+	if highscores_file.has_section("highscores"):
+		highscores_file.erase_section("highscores")
+
+	# Write each score entry: overall_score and all phase_scores
+	assert(highscores.size() == _MAX_NUM_ENTRIES)
+	for i in highscores.size():
+		var entry = highscores[i]
+		highscores_file.set_value("highscores", "entry_%d_score_value" % i, entry.score_value)
+		highscores_file.set_value("highscores", "entry_%d_name" % i, entry.name)
+
+	highscores_file.save(_HIGHSCORES_PATH)
+	print("Successfully saved highscores to file.")
--- src/end_screen/end_screen.gd
+++ src/end_screen/end_screen.gd
@@ -4,14 +4,28 @@ extends Control
 enum State
 {
 	TRANSITION_IN,
+	NEW_HIGHSCORE,
 	WAITING_FOR_INPUT,
 	TRANSITION_OUT,
 }
 
+const _NUM_INITIALS : int = 3
+const _MAX_INITIALS_IDX : int = _NUM_INITIALS + 1 # +1 for OK label.
+
 var _cur_state : State = State.TRANSITION_IN
 var _transition_time : float = 0.0
+var _is_new_highscore : bool = false
+
+var _selected_initial_idx : int = 0
+var _entered_name : String = "---"
 
 @onready var _transition_rect : TextureRect = $TransitionRect
+
+@onready var _new_highscore_screen : Control = %NewHighscoreScreen
+@onready var _statistics_screen : Control = %StatisticsScreen
+
+@onready var _initials : Array = [%Initial0, %Initial1, %Initial2]
+@onready var _ok_label : OKLabel = %OKLabel
 
 @onready var _score_label : Label = %Score
 @onready var _stages_cleared_label : Label = %StagesCleared
@@ -25,11 +39,25 @@ func _ready() -> void:
 
 	_set_statistics()
 
+	_is_new_highscore = Highscores.is_top_three_score(GameState.score)
+
+	if _is_new_highscore:
+		_new_highscore_screen.visible = true
+		_statistics_screen.visible = false
+	else:
+		_new_highscore_screen.visible = false
+		_statistics_screen.visible = true
+	
+	_selected_initial_idx = 0
+	_update_initials_selection()
+
 
 func _process(delta : float) -> void:
 	match _cur_state:
 		State.TRANSITION_IN:
 			_process_transition_in(delta)
+		State.NEW_HIGHSCORE:
+			_process_new_highscore(delta)
 		State.WAITING_FOR_INPUT:
 			_process_waiting_for_input(delta)
 		State.TRANSITION_OUT:
@@ -57,8 +85,51 @@ func _process_transition_in(delta : float) -> void:
 	_transition_time += delta
 	_transition_rect.material.set_shader_parameter("clear_progress", _transition_time)
 	if _transition_time >= 1.0:
-		_cur_state = State.WAITING_FOR_INPUT
+		_cur_state = State.NEW_HIGHSCORE if _is_new_highscore else State.WAITING_FOR_INPUT
 		_transition_time = 0.0
+
+
+func _process_new_highscore(_delta : float) -> void:
+	var fire_pressed : bool = Input.is_action_just_pressed("fire")
+	var jump_pressed : bool = Input.is_action_just_pressed("jump")
+	var ui_accept_pressed : bool = Input.is_action_just_pressed("ui_accept")
+	if (fire_pressed or jump_pressed or ui_accept_pressed) and _selected_initial_idx == _MAX_INITIALS_IDX - 1:
+		_entered_name = ""
+		for i in range(_NUM_INITIALS):
+			_entered_name += _initials[i].get_letter()
+
+		assert(_entered_name.length() == _NUM_INITIALS)
+		assert(Highscores.is_top_three_score(GameState.score))
+
+		var score_entry : Highscores.ScoreEntry = Highscores.ScoreEntry.new()
+		score_entry.score_value = GameState.score
+		score_entry.name = _entered_name
+		Highscores.add_score_entry(score_entry)
+
+		_new_highscore_screen.visible = false
+		_statistics_screen.visible = true
+		_cur_state = State.WAITING_FOR_INPUT
+
+	var ui_right_pressed : bool = Input.is_action_just_pressed("ui_right")
+	if ui_right_pressed or fire_pressed or jump_pressed or ui_accept_pressed:
+		_selected_initial_idx += 1
+		_selected_initial_idx = min(_selected_initial_idx, _MAX_INITIALS_IDX - 1)
+		_update_initials_selection()
+	
+	if Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_cancel"):
+		_selected_initial_idx -= 1
+		_selected_initial_idx = max(_selected_initial_idx, 0)
+		_update_initials_selection()
+	
+	if Input.is_action_just_pressed("ui_up"):
+		if _selected_initial_idx < _NUM_INITIALS:
+			assert(_initials[_selected_initial_idx].is_selected())
+			_initials[_selected_initial_idx].next_letter()
+	
+	if Input.is_action_just_pressed("ui_down"):
+		if _selected_initial_idx < _NUM_INITIALS:
+			assert(_initials[_selected_initial_idx].is_selected())
+			_initials[_selected_initial_idx].previous_letter()
 
 
 func _process_waiting_for_input(_delta : float) -> void:
@@ -66,6 +137,7 @@ func _process_waiting_for_input(_delta : float) -> void:
 	var jump_pressed : bool = Input.is_action_just_pressed("jump")
 	var fire_pressed : bool = Input.is_action_just_pressed("fire")
 	if accept_pressed or jump_pressed or fire_pressed:
+		Highscores.save_highscores_file()
 		_cur_state = State.TRANSITION_OUT
 
 
@@ -75,3 +147,12 @@ func _process_transition_out(delta : float) -> void:
 
 	if _transition_time >= 1.0:
 		Signals.scene_change_triggered.emit(SceneDefinitions.Scenes.TITLE_SCREEN)
+
+
+func _update_initials_selection() -> void:
+	assert(_selected_initial_idx >= 0 and _selected_initial_idx < _MAX_INITIALS_IDX)
+
+	for i in range(_NUM_INITIALS):
+		_initials[i].set_selected(i == _selected_initial_idx)
+
+	_ok_label.set_selected(_selected_initial_idx == _MAX_INITIALS_IDX - 1)
