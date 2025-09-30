cfbd3265f852553f668712740c6a5ee95b466e49
Oliver Naumann <bldrax@gmail.com>
2025-09-30 21:24:07 +0200

    Dont play end screen sounds when already left end screen

--- src/player/player_actor.gd
+++ src/player/player_actor.gd
@@ -13,6 +13,7 @@ const _FIRE_BALL_OFFSET_LEFT : Vector2 = Vector2(-8.0, -8.0)
 const _FIRE_BALL_OFFSET_RIGHT : Vector2 = Vector2(8.0, -8.0)
 
 var _cur_direction : Types.Direction = Types.Direction.RIGHT
+var _animation_tween : Tween = null
 var _animation_override : AnimationOverrides = AnimationOverrides.NONE
 
 @onready var _visuals : Node2D = $Visuals
@@ -34,22 +35,22 @@ func play_end_screen_sequence() -> void:
 	_sprite_head.play("normal")
 	_sprite_body.play("walk")
 
-	var animation_tween : Tween = get_tree().create_tween()
-	animation_tween.tween_interval(0.5)
-	animation_tween.tween_property(self, "velocity:x", 100.0, 0.0)
-	animation_tween.tween_interval(1.05)
-	animation_tween.tween_property(self, "velocity:x", 0.0, 0.0)
-	animation_tween.tween_interval(0.2)
-	animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_PLAYER_JUMP))
-	animation_tween.tween_callback(_set_velocity_y.bind(-140.0))
-	animation_tween.tween_interval(0.45)
-	animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_FIRE_SHOT))
-	animation_tween.tween_callback(_spawn_fire_ball)
-	animation_tween.tween_interval(0.25)
-	animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_FIRE_SHOT))
-	animation_tween.tween_callback(_spawn_fire_ball)
-	animation_tween.tween_interval(1.0)
-	animation_tween.tween_callback(_set_animation_override.bind(AnimationOverrides.CHEER))
+	_animation_tween = get_tree().create_tween()
+	_animation_tween.tween_interval(0.5)
+	_animation_tween.tween_property(self, "velocity:x", 100.0, 0.0)
+	_animation_tween.tween_interval(1.05)
+	_animation_tween.tween_property(self, "velocity:x", 0.0, 0.0)
+	_animation_tween.tween_interval(0.2)
+	_animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_PLAYER_JUMP))
+	_animation_tween.tween_callback(_set_velocity_y.bind(-140.0))
+	_animation_tween.tween_interval(0.45)
+	_animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_FIRE_SHOT))
+	_animation_tween.tween_callback(_spawn_fire_ball)
+	_animation_tween.tween_interval(0.25)
+	_animation_tween.tween_callback(SoundPool.play_sound.bind(SoundPool.SOUND_FIRE_SHOT))
+	_animation_tween.tween_callback(_spawn_fire_ball)
+	_animation_tween.tween_interval(1.0)
+	_animation_tween.tween_callback(_set_animation_override.bind(AnimationOverrides.CHEER))
 
 
 func _update_animation() -> void:
@@ -109,3 +110,9 @@ func _spawn_fire_ball() -> void:
 	else:
 		fire_ball.set_right()
 		fire_ball.global_position = global_position + _FIRE_BALL_OFFSET_RIGHT
+
+
+func _on_tree_exiting():
+	# Stop playing the tween.
+	if _animation_tween:
+		_animation_tween.kill()
