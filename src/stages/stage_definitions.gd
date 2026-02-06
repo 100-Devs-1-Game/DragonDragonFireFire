07cafa3f76f145625d3a329a910c55149b23d72d
Oliver Naumann <bldrax@gmail.com>
2026-02-06 21:06:13 +0100

    Add sweeper enemy and two news levels

--- src/stages/stage_definitions.gd
+++ src/stages/stage_definitions.gd
@@ -7,10 +7,12 @@ const HELLO_SHOOTER : PackedScene = preload("res://stages/hello_shooter.tscn")
 const THE_TOWER : PackedScene = preload("res://stages/the_tower.tscn")
 const SHOOTER_STAIRS : PackedScene = preload("res://stages/shooter_stairs.tscn")
 const ALL_TOGETHER : PackedScene = preload("res://stages/all_together.tscn")
+const HELLO_SWEEPER : PackedScene = preload("res://stages/hello_sweeper.tscn")
 const CRAZY_PLATFORMS : PackedScene = preload("res://stages/crazy_platforms.tscn")
 const SHOOTER_MADNESS : PackedScene = preload("res://stages/shooter_madness.tscn")
 const CRAZY_HOLES : PackedScene = preload("res://stages/crazy_holes.tscn")
 const BIG_BOX : PackedScene = preload("res://stages/big_box.tscn")
+const CROWDED_PLATFORMS : PackedScene = preload("res://stages/crowded_platforms.tscn")
 const PLATFORM_PUZZLE : PackedScene = preload("res://stages/platform_puzzle.tscn")
 const FINALE : PackedScene = preload("res://stages/finale.tscn")
 const BONUS : PackedScene = preload("res://stages/bonus.tscn")
@@ -22,11 +24,13 @@ const STAGES_DICT : Dictionary[int, PackedScene] = {
 	4: THE_TOWER,
 	5: SHOOTER_STAIRS,
 	6: ALL_TOGETHER,
-	7: CRAZY_PLATFORMS,
-	8: SHOOTER_MADNESS,
-	9: CRAZY_HOLES,
-	10: BIG_BOX,
-	11: PLATFORM_PUZZLE,
-	12: FINALE,
-	13: BONUS,
+	7 : HELLO_SWEEPER,
+	8: CRAZY_PLATFORMS,
+	9: SHOOTER_MADNESS,
+	10: CRAZY_HOLES,
+	11: BIG_BOX,
+	12: CROWDED_PLATFORMS,
+	13: PLATFORM_PUZZLE,
+	14: FINALE,
+	15: BONUS,
 }
